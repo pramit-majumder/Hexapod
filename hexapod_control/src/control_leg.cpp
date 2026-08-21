723422984f597ccc1ac6e2d46cf57c5b0c36bdc1
Waves_of_Shock <pramitmajumder404@gmail.com>
2026-08-21 23:06:07 +0530

    Working Interpolation Code, need input from topics

--- hexapod_control/src/control_leg.cpp
+++ hexapod_control/src/control_leg.cpp
@@ -12,33 +12,62 @@ public:
         x = 0;
         y = 150;
         z = 50;
-        timer = this->create_wall_timer(chrono::seconds(1), bind(&control_leg::callback, this));
+        timer = this->create_wall_timer(chrono::seconds(1), bind(&control_leg::traverse, this));
     }
 private:
-    void callback(){
-        
-        this->y += 10 * this->f;
-        RCLCPP_INFO(this->get_logger(), "Random: %d", 10 * this->f);
+    struct Point3d {
+        float x;
+        float y;
+        float z;
+    };
 
-        if(this->y > 225){
-            this->f = -1;
-        }
-        if( this->y < 150){
-            this->f = 1;
-        }
+    Point3d lerp(Point3d start, Point3d end, float t){
+        Point3d result;
+
+        result.x = start.x + ((end.x - start.x) * t);
+        result.y = start.y + ((end.y - start.y) * t);
+        result.z = start.z + ((end.z - start.z) * t);
+        
+        return result;
+    }
+    
+    void traverse(){
+        
         auto node = hexapod_interfaces::msg::Coordinates();
-        node.x = this->x;
-        node.y = this->y;
-        node.z = this->z;
-        RCLCPP_INFO(this->get_logger(), "X: %d", this->x);
-        RCLCPP_INFO(this->get_logger(), "Y: %d", this->y);
-        RCLCPP_INFO(this->get_logger(), "Z: %d", this->z);
-        this->coords->publish(node);
+        
+        Point3d start = {(float)this->x, (float)this->y, (float)this->z};
+        Point3d end = {node.x, node.y, node.z};
+
+        for(int i = 0; i <= 50; i++){
+            
+            Point3d result = this->lerp(start, end, (float)i/50);
+            node.x = result.x;
+            node.y = result.y;
+            node.z = result.z;
+        
+            this->coords->publish(node);
+            RCLCPP_INFO(this->get_logger(), "X: %d", node.x);
+            RCLCPP_INFO(this->get_logger(), "Y: %d", node.y);
+            RCLCPP_INFO(this->get_logger(), "Z: %d", node.z);
+        }
 
     }
+
+    // void callback(){
+        
+        
+        
+    //     RCLCPP_INFO(this->get_logger(), "X: %d", this->x);
+    //     RCLCPP_INFO(this->get_logger(), "Y: %d", this->y);
+    //     RCLCPP_INFO(this->get_logger(), "Z: %d", this->z);
+    //     this->coords->publish(node);
+
+    // }
+
     rclcpp::TimerBase::SharedPtr timer;
     int counter;
     int x, y, z;
+    // int prev_x, prev_y, prev_z;
     int f = 1;
     rclcpp::Publisher<hexapod_interfaces::msg::Coordinates>::SharedPtr coords;
 };
@@ -50,3 +79,81 @@ int main(int argc, char **argv){
     rclcpp::spin(node);
     rclcpp::shutdown();
 }
+
+// import numpy as np
+
+
+// def lerp(start, end, t):
+
+//     start = np.array(start, dtype=float)
+//     end = np.array(end, dtype=float)
+
+//     return start + (end - start) * t
+
+
+// def generate_trajectory(start, end, steps=50):
+//     trajectory = []
+
+//     for i in range(steps + 1):
+//         t = i / steps
+//         point = lerp(start, end, t)
+//         trajectory.append(point)
+
+//     return trajectory
+
+
+// start_position = [0, 150, -80]
+// end_position   = [0, 150, 160]
+
+// trajectory = generate_trajectory(
+//     start_position,
+//     end_position,
+//     steps=50
+// )
+
+// for point in trajectory:
+//     print(
+//         f"x={point[0]:.2f}, "
+//         f"y={point[1]:.2f}, "
+//         f"z={point[2]:.2f}"
+//     )
+
+// struct Point3D {
+//     float x;
+//     float y;
+//     float z;
+// };
+
+
+// Point3D lerp(Point3D start, Point3D end, float t)
+// {
+//     Point3D result;
+
+//     result.x = start.x + (end.x - start.x) * t;
+//     result.y = start.y + (end.y - start.y) * t;
+//     result.z = start.z + (end.z - start.z) * t;
+
+//     return result;
+// }
+
+
+// int main()
+// {
+//     Point3D start = {100, 0, -80};
+//     Point3D end   = {150, 0, -80};
+
+//     for(int i = 0; i <= 50; i++)
+//     {
+//         float t = (float)i / 50;
+
+//         Point3D foot = lerp(start, end, t);
+
+//         std::cout 
+//             << "X: " << foot.x
+//             << " Y: " << foot.y
+//             << " Z: " << foot.z
+//             << std::endl;
+//     }
+
+//     return 0;
+// }
